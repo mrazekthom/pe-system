974baf7fbefca26824f434b6b0489e54afb3313b
Mrázek Tomáš <mrazekthom@gmail.com>
2015-10-21 22:25:35 +0200

    added MagicAccessors

--- app/model/Entity/Sport.php
+++ app/model/Entity/Sport.php
@@ -3,13 +3,14 @@
 namespace App\Model\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Kdyby\Doctrine\Entities\MagicAccessors;
 
 /**
  * @ORM\Entity
  */
 class Sport
 {
-
+    use MagicAccessors;
     use \Kdyby\Doctrine\Entities\Attributes\Identifier;
 
     /**
--- app/model/Entity/Student.php
+++ app/model/Entity/Student.php
@@ -3,14 +3,16 @@
 namespace App\Model\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Kdyby\Doctrine\Entities\MagicAccessors;
 
 /**
  * @ORM\Entity
  */
 class Student
 {
-
+    use MagicAccessors;
     use \Kdyby\Doctrine\Entities\Attributes\Identifier;
+
 
     /**
      * @ORM\ManyToOne(targetEntity="SchoolClass")
