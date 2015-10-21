e3989c14afd6d04260ca8e8b1d4158963bd42c70
Mrázek Tomáš <mrazekthom@gmail.com>
2015-10-21 22:28:01 +0200

    added StudentQuery to cleaver make DQL query

--- app/AdministrationModule/presenters/HomepagePresenter.php
+++ app/AdministrationModule/presenters/HomepagePresenter.php
@@ -2,12 +2,39 @@
 
 namespace App\AdministrationModule\Presenters;
 
+use App\Model\Entity\Student;
+use App\Model\EntityService\StudentQuery;
+
 
 /**
  * Homepage presenter for Administration Module.
  */
 class HomepagePresenter extends BaseAdministrationPresenter
 {
+
+    /**
+     * @inject
+     * @var \Kdyby\Doctrine\EntityManager
+     */
+    public $EM;
+
+    /** @var StudentQuery @inject */
+    public $SQ;
+
+    public function renderDefault()
+    {
+        //Only for testing
+        $query = new StudentQuery();
+        $query->setClass('4A');
+        $result = $this->EM->getRepository(Student::class)->fetch($query);
+        $foo = $result;
+        foreach ($foo as $bar) {
+            dump($bar);
+        }
+
+        exit();
+        //Only for testing
+    }
 
 
 }
---
+++ app/model/EntityService/StudentQuery.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Model\EntityService;
+
+use App\Model\Entity\Grade;
+use App\Model\Entity\SchoolClass;
+use App\Model\Entity\SchoolYear;
+use App\Model\Entity\Student;
+use App\Model\Entity\TypeClass;
+use Doctrine\ORM\Query\Expr\Join;
+use Kdyby\Doctrine\QueryObject;
+use Kdyby\Persistence\Queryable;
+use Nette\Utils\Strings;
+
+class StudentQuery extends QueryObject
+{
+
+    CONST ACTUAL = 1;
+
+    private $typeClass = NULL;
+    private $grade = NULL;
+    private $educationDay = NULL;
+
+    public function setClass($class)
+    {
+        $this->grade = Strings::match($class, '~[0-9]~')[0];
+        $this->typeClass = Strings::match($class, '~[A-Z]~')[0];
+    }
+
+    public function setEducationDay($day)
+    {
+        $this->educationDay = $day; //TODO: security type day
+    }
+
+    public function setGrade($grade)
+    {
+        $this->grade = Strings::match($grade, '~[0-9]~')[0];
+    }
+
+    public function getActualDay()
+    {
+        $this->educationDay = date('l');
+    }
+
+
+    /**
+     * @param \Kdyby\Persistence\Queryable $repository
+     * @return \Doctrine\ORM\Query|\Doctrine\ORM\QueryBuilder
+     */
+    protected function doCreateQuery(Queryable $repository)
+    {
+        $qb = $repository->createQueryBuilder()
+            ->select('s')->from(Student::class, 's')
+            ->innerJoin(SchoolClass::class, 'c', Join::WITH, 's.class = c')
+            ->innerJoin(Grade::class, 'g', Join::WITH, 'c.grade = g')
+            ->innerJoin(SchoolYear::class, 'y', Join::WITH, 'g.schoolYear = y')
+            ->innerJoin(TypeClass::class, 't', Join::WITH, 'c.typeClass = t')
+            ->andWhere('y.actual = :year')
+            ->setParameter('year', $this::ACTUAL);
+
+        if (isset($this->typeClass)) {
+            $qb->andWhere('t.class = :typeClass')
+                ->setParameter('typeClass', $this->typeClass);
+        }
+        if (isset($this->grade)) {
+            $qb->andWhere('g.grade = :grade')
+                ->setParameter('grade', $this->grade);
+        }
+        if (isset($this->educationDay)) {
+            $qb->setParameter('typeClass', $this->typeClass)
+                ->setParameter('educationDay', $this->educationDay);
+        }
+
+        return $qb;
+    }
+
+}
